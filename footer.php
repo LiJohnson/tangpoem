8d59923291e770e1015f3339dac56dcf6c6296b9
LiJohnson <598420668@163.com>
2014-10-28 22:34:15 +0800

    改用图标

--- footer.php
+++ footer.php
@@ -5,14 +5,15 @@
 				V 1.0.1
 			</li>
 			<li>
-				<a href='http://www.weibo.com/lijohnson' target="_blank" >微博</a>
+				<a href='http://www.weibo.com/lijohnson' class='genericon-user' target="_blank" ></a>
 			</li>
 			<li>
-				<a href='https://github.com/LiJohnson/' target="_blank" >GitHub</a>
+				<a href='https://github.com/LiJohnson/' class='genericon-github' target="_blank" ></a>
 			</li>
 			<li>
-				有什么想说的可以<a href="javascript:;" click-feedback >点这里</a>
+				有什么想说的可以<a href="javascript:;" click-feedback >点这里<i class='genericon-chat' ></i></a>
 			</li>
 		</ul>
 	<div>
 </footer>
+<link rel="stylesheet" type="text/css" href="http://gtbcode.sinaapp.com/css/icon.css">
