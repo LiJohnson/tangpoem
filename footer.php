2de5d93ae306584ffd63dc96d5efe6a52992cbe8
LiJohnson <598420668@163.com>
2014-10-30 22:03:39 +0800

    改用font-awesome图标

--- footer.php
+++ footer.php
@@ -2,13 +2,13 @@
 	<div class="container text-center" style="margin-top:100px">
 		<ul class="list-inline" >
 			<li>
-				V 1.0.1
+				<small>V 1.0.1</small>
 			</li>
 			<li>
-				<a href='http://www.weibo.com/lijohnson' class='ion-android-contact' target="_blank" ></a>
+				<a href='http://www.weibo.com/lijohnson' class='fa fa-weibo' target="_blank" ></a>
 			</li>
 			<li>
-				<a href='https://github.com/LiJohnson/tangpoem' class='ion-social-github' target="_blank" ></a>
+				<a href='https://github.com/LiJohnson/tangpoem' class='fa fa-github' target="_blank" ></a>
 			</li>
 			<li>
 				有什么想说的可以<a href="javascript:;" click-feedback >点这里<i class='genericon-chat' ></i></a>
@@ -16,4 +16,4 @@
 		</ul>
 	<div>
 </footer>
-<link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
